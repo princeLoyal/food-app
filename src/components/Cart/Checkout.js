1486fe8e8092d861ae5963d4489c7e8ceb5a1886
princeLoyal <princeloyal1234@gmail.com>
2023-01-15 10:42:55 +0100

    gccgd

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -54,17 +54,23 @@ const Checkout = props => {
                 <input type='text' id='name' ref={nameInputRef}/>
                 {!formValid.name && <p>please input a valid name</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} 
+            ${!formValid.street && classes.invalid}
+            `}>
                 <label htmlFor='street'>Street</label>
                 <input type='text' id='street' ref={streetInputRef}/>
                 {!formValid.street && <p>please input a valid street</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} 
+            ${!formValid.postal && classes.invalid}
+            `}>
                 <label htmlFor='postal'>Postal Code</label>
                 <input type='text' id='postal' ref={postalInputRef}/>
-                {!formValid.postal && <p>please input a valid postal code (length === 5)</p>}
+                {!formValid.postal && <p>please input a valid postal code (length = 5)</p>}
             </div>
-            <div className={classes.control}>
+            <div className={`${classes.control} 
+            ${!formValid.city && classes.invalid}
+            `}>
                 <label htmlFor='city'>City</label>
                 <input type='text' id='city' ref={cityInputRef}/>
                 {!formValid.city && <p>please input a valid city</p>}
